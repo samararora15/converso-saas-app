31f508919130bfa3f3e644db5440897c26547dce
samararora15 <officialsamar404@gmail.com>
2025-06-10 15:35:15 +0530

    build update

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -25,8 +25,8 @@ const NewCompanion = async () => {
                         <div className="cta-badge">
                             Upgrade your plan
                         </div>
-                        <h1>You've Reached Your Limit</h1>
-                        <p>You've reached your companion limit. Upgrade to create more companions and premium features.</p>
+                        <h1>You&apos;ve Reached Your Limit</h1>
+                        <p>You&apos;ve reached your companion limit. Upgrade to create more companions and premium features.</p>
                         <Link href="/subscription" className="btn-primary w-full justify-center" >
                             Upgrade My Plan
                         </Link>
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,7 +1,6 @@
 'use server';
 import { auth } from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase";
-import { revalidatePath } from "next/cache";
 
 export const createCompanion = async(formData:CreateCompanion) => {
     const {userId:author} = await auth();
